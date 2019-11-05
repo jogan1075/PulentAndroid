0e1ce62d3e781534a93be072636dfb6639d9ff0b
Jonathan Muñoz <jonathan1075@gmail.com>
2019-11-05 16:48:35 -0300

    se modifican y agregan funciones del mapeo de datos de las canciones

--- app/src/main/java/com/jmc/pulentandroid/utils/mappers.kt
+++ app/src/main/java/com/jmc/pulentandroid/utils/mappers.kt
@@ -1,15 +1,20 @@
 package com.jmc.pulentandroid.utils
 
+import com.jmc.pulentandroid.R
 import com.jmc.pulentandroid.data.entity.api.AlbumEntity
 import com.jmc.pulentandroid.domain.model.Artist
 import com.jmc.pulentandroid.data.entity.api.ArtistEntity
+import com.jmc.pulentandroid.data.entity.api.TrackEntity
 import com.jmc.pulentandroid.domain.model.Album
+import com.jmc.pulentandroid.domain.model.Track
+import com.jmc.pulentandroid.presentation.modelView.TrackItem
 
 /**
  * Created by Jmunoz on 2019-10-31.
  */
 
 /* from data to domain layer */
+
 
 fun ArtistEntity.toArtist() = Artist(
     artistId = artistId,
@@ -39,4 +44,117 @@ fun AlbumEntity.toAlbum() = Album(
     currency = currency,
     releaseDate = releaseDate.parseISO8601Date(),
     primaryGenreName = primaryGenreName
+)
+
+fun TrackEntity.toTrack() = Track(
+    artistId = artistId,
+    collectionId = collectionId,
+    trackId = trackId,
+    artistName = artistName,
+    collectionName = collectionName,
+    trackName = trackName,
+    collectionCensoredName = collectionCensoredName,
+    trackCensoredName = trackCensoredName,
+    artistViewUrl = artistViewUrl,
+    collectionViewUrl = collectionViewUrl,
+    trackViewUrl = trackViewUrl,
+    previewUrl = previewUrl,
+    collectionPrice = collectionPrice,
+    trackPrice = trackPrice,
+    releaseDate = releaseDate,
+    isCollectionExplicit = collectionExplicitness != "notExplicit",
+    isTrackExplicit = trackExplicitness != "notExplicit",
+    discCount = discCount,
+    discNumber = discNumber,
+    trackCount = trackCount,
+    trackNumber = trackNumber,
+    trackTimeMillis = trackTimeMillis,
+    country = country,
+    currency = currency,
+    primaryGenreName = primaryGenreName,
+    isStreamable = isStreamable,
+    isDownloading = false,
+    isPlaying = false,
+    isPaused = false,
+    isDownloaded = false,
+    isMusic = previewUrl.endsWith(".m4a"),
+    isVideo = previewUrl.endsWith(".m4v")
+)
+
+
+/* from domain to presentation layer */
+
+fun Track.toTrackItem() = TrackItem(
+    artistId = artistId,
+    collectionId = collectionId,
+    trackId = trackId,
+    artistName = artistName,
+    collectionName = collectionName,
+    trackName = trackName,
+    collectionCensoredName = collectionCensoredName,
+    trackCensoredName = trackCensoredName,
+    artistViewUrl = artistViewUrl,
+    collectionViewUrl = collectionViewUrl,
+    trackViewUrl = trackViewUrl,
+    previewUrl = previewUrl,
+    collectionPrice = collectionPrice,
+    trackPrice = trackPrice,
+    releaseDate = releaseDate,
+    isCollectionExplicit = isCollectionExplicit,
+    isTrackExplicit = isTrackExplicit,
+    discCount = discCount,
+    discNumber = discNumber,
+    trackCount = trackCount,
+    trackNumber = trackNumber,
+    trackTimeMillis = trackTimeMillis,
+    country = country,
+    currency = currency,
+    primaryGenreName = primaryGenreName,
+    isStreamable = isStreamable,
+    isDownloading = isDownloading,
+    isPlaying = isPlaying,
+    isPaused = isPaused,
+    isDownloaded = isDownloaded,
+    isMusic = isMusic,
+    isVideo = isVideo,
+    /* Presentation fields */
+    nameIconResource = if (isTrackExplicit) R.drawable.ic_explicit else 0,
+    timeMillisString = trackTimeMillis.formatInterval()
+)
+
+/* from presentation to domain layer */
+
+fun TrackItem.toTrack() = Track(
+    artistId = artistId,
+    collectionId = collectionId,
+    trackId = trackId,
+    artistName = artistName,
+    collectionName = collectionName,
+    trackName = trackName,
+    collectionCensoredName = collectionCensoredName,
+    trackCensoredName = trackCensoredName,
+    artistViewUrl = artistViewUrl,
+    collectionViewUrl = collectionViewUrl,
+    trackViewUrl = trackViewUrl,
+    previewUrl = previewUrl,
+    collectionPrice = collectionPrice,
+    trackPrice = trackPrice,
+    releaseDate = releaseDate,
+    isCollectionExplicit = isCollectionExplicit,
+    isTrackExplicit = isTrackExplicit,
+    discCount = discCount,
+    discNumber = discNumber,
+    trackCount = trackCount,
+    trackNumber = trackNumber,
+    trackTimeMillis = trackTimeMillis,
+    country = country,
+    currency = currency,
+    primaryGenreName = primaryGenreName,
+    isStreamable = isStreamable,
+    isDownloading = isDownloading,
+    isPlaying = isPlaying,
+    isPaused = isPaused,
+    isDownloaded = isDownloaded,
+    isMusic = isMusic,
+    isVideo = isVideo
 )
