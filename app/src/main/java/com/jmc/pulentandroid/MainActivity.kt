1b42e7ca2fa67e0648694c87d541ab77e4243540
Jonathan Muñoz <jonathan1075@gmail.com>
2019-11-05 15:44:25 -0300

    commit initial

---
+++ app/src/main/java/com/jmc/pulentandroid/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.jmc.pulentandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jmc.pulentandroid.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/jmc/pulentandroid/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.jmc.pulentandroid.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
