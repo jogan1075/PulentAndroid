c07ce41a8176b524481e81e5785f0cdb5c81beb1
Jonathan Muñoz <jonathan1075@gmail.com>
2019-11-05 16:28:02 -0300

    se agrega el mapper correspondiente para los albumes

--- app/src/main/java/com/jmc/pulentandroid/utils/mappers.kt
+++ app/src/main/java/com/jmc/pulentandroid/utils/mappers.kt
@@ -1,7 +1,9 @@
 package com.jmc.pulentandroid.utils
 
+import com.jmc.pulentandroid.data.entity.api.AlbumEntity
 import com.jmc.pulentandroid.domain.model.Artist
 import com.jmc.pulentandroid.data.entity.api.ArtistEntity
+import com.jmc.pulentandroid.domain.model.Album
 
 /**
  * Created by Jmunoz on 2019-10-31.
@@ -16,4 +18,25 @@ fun ArtistEntity.toArtist() = Artist(
     primaryGenreName = primaryGenreName ?: "",
     primaryGenreId = primaryGenreId ?: 0,
     isLiked = false
+)
+
+fun AlbumEntity.toAlbum() = Album(
+    collectionId = collectionId,
+    artistId = artistId,
+    artistName = artistName,
+    collectionName = collectionName,
+    collectionCensoredName = collectionCensoredName,
+    artistViewUrl = artistViewUrl,
+    collectionViewUrl = collectionViewUrl,
+    artworkUrl30 = artworkUrl30 ?: "",
+    artworkUrl60 = artworkUrl60 ?: "",
+    artworkUrl100 = artworkUrl100 ?: "",
+    collectionPrice = collectionPrice,
+    isExplicit = collectionExplicitness != "notExplicit",
+    trackCount = trackCount,
+    copyright = copyright,
+    country = country,
+    currency = currency,
+    releaseDate = releaseDate.parseISO8601Date(),
+    primaryGenreName = primaryGenreName
 )
